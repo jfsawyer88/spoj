67367c66d1dac962cc0e0cb8efa479e98f0ed14c
Jorge <jfsawyer88@gmail.com>
2015-10-03 17:01:57 +0000

    First Commit

---
+++ 0001-42/42.py
@@ -0,0 +1,12 @@
+## SPOJ
+## Life, the Universe, and Everything
+
+import sys
+
+s = sys.stdin
+n = s.readline()
+sys.stdout.write(n)
+
+while n != '42\n':
+    n = s.readline()
+    sys.stdout.write(n)
